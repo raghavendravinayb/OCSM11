dd18e6e19ca2621182b06a89595e56d08db3c74a
Raghav <Raghav@Raghav>
2020-09-15 09:09:47 +0530

    Project Commit

---
+++ Actitime/src/test/java/com/actitime/timetrack/Demo.java
@@ -0,0 +1,13 @@
+package com.actitime.timetrack;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void run()
+	{
+		Reporter.log("Maven Tests Successully running",true);
+	}
+}
---
+++ Actitime/src/test/java/com/actitime/timetrack/Remo.java
@@ -0,0 +1,13 @@
+package com.actitime.timetrack;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Remo {
+
+	@Test
+	public void run()
+	{
+		Reporter.log("Remo Tests Successully running",true);
+	}
+}
