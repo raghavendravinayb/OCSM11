d5459e103f855b550cc348a29d90958609394f0b
Raghav <Raghav@Raghav>
2020-10-11 09:04:27 +0530

    Sumo

---
+++ Actitime/src/test/java/com/actitime/timetrack/Sumo.java
@@ -0,0 +1,13 @@
+package com.actitime.timetrack;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Sumo {
+
+	@Test
+	public void run()
+	{
+		Reporter.log("Maven Tests Successully running",true);
+	}
+}
